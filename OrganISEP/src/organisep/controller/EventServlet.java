b58359edba9f2d819e5b4094f35b7e80d20443f9
shca10766 <shirley.caillere@gmail.com>
2019-05-31 09:28:20 +0200

    javax mail

--- OrganISEP/src/organisep/controller/EventServlet.java
+++ OrganISEP/src/organisep/controller/EventServlet.java
@@ -1,7 +1,6 @@
 package organisep.controller;
 
 import java.io.IOException;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
